76e61777beed132a5670d58b0586455d7c344f1a
Mansi Gupta <mansi.gupta@codesquaretech.com>
2022-07-13 18:13:55 +0530

    updated detailsPage

--- detailsPage.js
+++ detailsPage.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $('#header').load('topHEader.html');
+        $('#header').load('topHeader.html');
         $('#footer').load('footer.html');
 
         // $('#productRoute').text(`${cat_name} > ${sub_cat_name} `);
@@ -12,12 +12,17 @@ $(document).ready(function(){
         $('#discount').html(`-50% &nbsp `);
         // $('#discount').html(`-${product_discount_percentage}% &nbsp `);
          
-        $('#price').html(`<span id="currency">&#8377</span><span id="product_price">499.00</span>`);
-        // $('#price').html(`<span id="currency" >&#8377</span>${product_discount_price}`);
+        $('.price').html(`<span id="currency">&#8377</span><span id="product_price">499.00</span>`);
+        // $('.price').html(`<span id="currency" >&#8377</span>${product_discount_price}`);
 
         $('#mrp').html(`M.R.P.: <s>&#8377 899.00</s>`);
         // $('#mrp').html(`M.R.P.: <s>&#8377 ${product_price}</s>`);
 
+        $('#color-name').html(`<strong>Color Name </strong>: Yellow`);
+        // $('#color-name').html(`Color Name :  ${product_color}`);
+
+        $('#product_description').html(`<strong>Description</strong> : <br> The outsoles are made by an air cushion, doubling the effect of shock absorption. Besides, these shoes perform excellent in durability and are also slip resistant. It provides push cushioning comfort for foot pain relief and helps relieve pressure while conforming to your every step`);
+        // $('#product_description').html(`Description :  ${product_description}`);
 
 })
 
