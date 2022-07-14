2b5f0a793dbe69182f75508920766500a9aa1575
sandeep-csqt <sandeep.vishwakarma@codesquaretech.com>
2022-07-14 17:36:41 +0530

    s06

--- public/js/topheader.js
+++ public/js/topheader.js
@@ -4,8 +4,18 @@ console.log(subcategory_data);
     $("#hiddenLi").hide();
 
     $("#mens_cat").mouseenter(function () {
-
+            var lst_str = '';
             var cat_id = $(this).val();
+            for(let i=0; i<subcategory_data.length;i++){
+                console.log("getting list", subcategory_data[i]);
+                if(subcategory_data[i].cat_id == cat_id ){
+                    lst_str += `<li style="font-family:cursive ;"><b><a href="product-listing.html?cat_id=${cat_id}&sub_cat_id=${subcategory_data[i].sub_cat_id}">${subcategory_data[i].sub_cat_name}</a> </b></li>`
+                    
+                }
+              
+            }
+            $('#cat_list').html(lst_str);
+        
             
         $('#hiddenLi').css('display' , 'flex')
         $("#hiddenLi").show();
@@ -35,6 +45,18 @@ console.log(subcategory_data);
     $("#hiddenLi2").hide();
 
     $("#women_cat").mouseenter(function () {
+        var lst_str1 = '';
+            var cat_id1 = $(this).val();
+            for(let i=0; i<subcategory_data.length;i++){
+                console.log("getting list", subcategory_data[i]);
+                if(subcategory_data[i].cat_id == cat_id1 ){
+                    lst_str1 += `<li style="font-family:cursive ;"><b><a href="product-listing.html?cat_id=${cat_id}&sub_cat_id=${subcategory_data[i].sub_cat_id}">${subcategory_data[i].sub_cat_name}</a> </b></li>`
+                    
+                }
+              
+            }
+            $('#women_cat_list').html(lst_str1);
+
         $('#hiddenLi2').css('display' , 'flex')
         $("#hiddenLi2").show();
     })
@@ -62,6 +84,7 @@ console.log(subcategory_data);
     });
 
 
+ 
 });
 
 //mens_cat
