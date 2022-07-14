27a4e35827973a79b402cae03908b84ef6b570f9
aafreenkhancode <aafreen.khan@codesquaretech.com>
2022-07-14 17:21:33 +0530

    aashu

--- public/js/product-listing.js
+++ public/js/product-listing.js
@@ -1,11 +1,18 @@
 
 // console.log(subcategory_data);
 // sitepoint
+$(document).ready(function(){
+
+  $('#header').load('topHEader.html');
+  $('#footer').load('footer.html');
+
+
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-// const cat_id = urlParams.get('cate_id');
-// const sub_category_id = urlParams.get('sub_cat_id');
+
+const param_cat_id = urlParams.get('cate_id');
+const param_sub_category_id = urlParams.get('sub_cat_id');
 // console.log(cat_id,
 //   sub_category_id);
   // jjgjg
@@ -15,12 +22,21 @@ const urlParams = new URLSearchParams(queryString);
   //   ).innerHTML += `<p style="font-size:12px; color:#777777;"><b>${category_data[i].cat_id}</b  > : ${category_data[i].sub_cat_id} </p>`;
   // }
 
-  category_data.forEach(function(value , index){
-    console.log(value);
+  // category_data.forEach(function(value , index){
+  //   console.log(value);
+  // });
+ 
+  {
+  Product_data.forEach(function(value , index){
+    if(value.cate_id == param_cat_id && value.sub_cat_id == param_sub_category_id){
+      console.log(value);
+    }
   });
-  subcategory_data.forEach(function(value , index){
-    console.log(value);
-  });
+}
+
+
+
+})
+
+
 // shirt
-
-
