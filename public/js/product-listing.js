b89f89126e2a182eb9320c05f17ded9178f803db
aafreenkhancode <aafreen.khan@codesquaretech.com>
2022-07-15 09:56:12 +0530

    aafreen

--- public/js/product-listing.js
+++ public/js/product-listing.js
@@ -17,39 +17,38 @@ const param_cat_id = urlParams.get('cate_id');
 const param_sub_category_id = urlParams.get('sub_cat_id');
 // console.log(cat_id,
 //   sub_category_id);
-  // jjgjg
-  // for (let i = 0; i < category_data.length; i++) {
-  //   console.log("dsdsd", category_data[i]);
-  //   document.getElementById("footer"
-  //   ).innerHTML += `<p style="font-size:12px; color:#777777;"><b>${category_data[i].cat_id}</b  > : ${category_data[i].sub_cat_id} </p>`;
-  // }
 
   // category_data.forEach(function(value , index){
   //   console.log(value);
   // });
  
-//   {
   Product_data.forEach(function(value , index){
     
     if(value.cate_id == param_cat_id && value.sub_cat_id == param_sub_category_id){
       console.log(value.sub_cat_id);
         product_listing_details.push(value);
-        $('#listing').append(` <div class="card" style="width: 15rem; margin-right:1%;">
+        $('#listing').append(`<div value = "${value.product_id}" class="card m-2 product-details-page" style="width: 15rem; margin-right:1%;">
         <img class="card-img-top" src="${value.product_img}"
             alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">₹${value.product_price}</h5>
             <p class="card-text"><del>₹${value.product_discount_price} </del> <br> ${value.product_description}
                 Shirt <br><i class="fa-solid fa-star" style="font-size: smaller;"></i><i
-                    class="fa-solid fa-star" style="font-size: smaller;"></i><i class="fa-solid fa-star"
-                    style="font-size: smaller;"></i><i class="fa-solid fa-star"
+                    class="fa-solid fa-star" style="font-size: smaller;"></i>
+                    <i class="fa-solid fa-star" style="font-size: smaller;"></i><i class="fa-solid fa-star"
                     style="font-size: smaller;"></i><i class="fa-regular fa-star"
-                    style="font-size: smaller;"></i> 15,843</p>
+                    style="font-size: smaller;"></i><br>${value.product_rating_count}</p>
         </div>
     </div>`)
     }
     
   });
+
+  $('.product-details-page').on('click' , function(){
+    var p_id = $(this).attr('value');
+    // alert(p_id)
+    window.location.href = `detailsPage.html?product_id=${p_id}`;
+  })
 
   
   console.log(product_listing_details);
