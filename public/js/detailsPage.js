bd7f7ff4569222c8d34502cb8d392310c6bbb51c
aashu89 <aashutosh.namdeo@codesquaretech.com>
2022-07-20 00:29:23 +0530

    aashu

--- public/js/detailsPage.js
+++ public/js/detailsPage.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
   $("#mrp").html(`M.R.P.: <s>&#8377 ${details[0].product_price}</s>`);
 
   $("#color-name").html(`Color Name :  ${details[0].product_color}`);
-var color= (details[0].product_color).toLowerCase();
+  var color= (details[0].product_color).toLowerCase();
   $('#color-bubble').css("background-color",color);
 
   $("#product_description").html(
