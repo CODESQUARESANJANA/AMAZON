6595269f8cdd1a153e0601279eac2827a5de7cae
ranibarkhane <rani.barkhane@codesquaretech.com>
2022-07-21 23:21:02 +0530

    rani

--- public/js/kids.js
+++ public/js/kids.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
 	$('#header').load('topHeader.html');
 	$('#footer').load('footer.html');
+	let percentageArr = [];
+	// Product_data.forEach(function (value, index){
+
+	// 	if(value.cat_id==3){
+	// 		if(value.product_discount_percenteage>=10 && value.product_discount_percenteage<=25){
+	// 			console.log('value.product_id',value.product_id,value.product_discount_percenteage);
+	// 			percentageArr.push(value.product_id);
+	// 	// 		$('#percentageFilter').append(`
+	// 	// <li><a class="listValue" href="detailsPage.html?product_id=${value.product_id}">${value.product_discount_percenteage}</a></li>`)
+	// 		}
+	// 		else if(value.product_discount_percenteage>10 && value.product_discount_percenteage<25){
+	// 			console.log('value.product_id',value.product_id,value.product_discount_percenteage);
+	// 			percentageArr.push(value.product_id);
+	// 	// 		$('#percentageFilter').append(`
+	// 	// <li><a class="listValue" href="detailsPage.html?product_id=${value.product_id}">${value.product_discount_percenteage}</a></li>`)
+	// 		}
+	// 	}
+
+
+
+
+	// })
 })
 
 
@@ -62,48 +84,48 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
 		}
 	}
 });
-$('#carousel-example1').on('slide.bs.carousel', function (e) {
-	/*
-		CC 2.0 License Iatek LLC 2018 - Attribution required
-	*/
-	var e = $(e.relatedTarget);
-	var idx = e.index();
-	var itemsPerSlide = 6;
-	var totalItems = $('.carousel-item').length;
+// $('#carousel-example1').on('slide.bs.carousel', function (e) {
+// 	/*
+// 		CC 2.0 License Iatek LLC 2018 - Attribution required
+// 	*/
+// 	var e = $(e.relatedTarget);
+// 	var idx = e.index();
+// 	var itemsPerSlide = 6;
+// 	var totalItems = $('.carousel-item').length;
 
-	if (idx >= totalItems - (itemsPerSlide - 1)) {
-		var it = itemsPerSlide - (totalItems - idx);
-		for (var i = 0; i < it; i++) {
-			// append slides to end
-			if (e.direction == "left") {
-				$('.carousel-item').eq(i).appendTo('.carousel-inner');
-			}
-			else {
-				$('.carousel-item').eq(0).appendTo('.carousel-inner');
-			}
-		}
-	}
-});
-$('#carousel-example2').on('slide.bs.carousel', function (e) {
+// 	if (idx >= totalItems - (itemsPerSlide - 1)) {
+// 		var it = itemsPerSlide - (totalItems - idx);
+// 		for (var i = 0; i < it; i++) {
+// 			// append slides to end
+// 			if (e.direction == "left") {
+// 				$('.carousel-item').eq(i).appendTo('.carousel-inner');
+// 			}
+// 			else {
+// 				$('.carousel-item').eq(0).appendTo('.carousel-inner');
+// 			}
+// 		}
+// 	}
+// });
+// $('#carousel-example2').on('slide.bs.carousel', function (e) {
 
-	var e = $(e.relatedTarget);
-	var idx = e.index();
-	var itemsPerSlide = 6;
-	var totalItems = $('.carousel-item').length;
+// 	var e = $(e.relatedTarget);
+// 	var idx = e.index();
+// 	var itemsPerSlide = 6;
+// 	var totalItems = $('.carousel-item').length;
 
-	if (idx >= totalItems - (itemsPerSlide - 1)) {
-		var it = itemsPerSlide - (totalItems - idx);
-		for (var i = 0; i < it; i++) {
-			// append slides to end
-			if (e.direction == "left") {
-				$('.carousel-item').eq(i).appendTo('.carousel-inner');
-			}
-			else {
-				$('.carousel-item').eq(0).appendTo('.carousel-inner');
-			}
-		}
-	}
-});
+// 	if (idx >= totalItems - (itemsPerSlide - 1)) {
+// 		var it = itemsPerSlide - (totalItems - idx);
+// 		for (var i = 0; i < it; i++) {
+// 			// append slides to end
+// 			if (e.direction == "left") {
+// 				$('.carousel-item').eq(i).appendTo('.carousel-inner');
+// 			}
+// 			else {
+// 				$('.carousel-item').eq(0).appendTo('.carousel-inner');
+// 			}
+// 		}
+// 	}
+// });
 
 function myFunction3() {
 	var x = document.getElementById("mysidepnav");
@@ -115,226 +137,107 @@ function myFunction3() {
 }
 
 
+// let offer10Arr = [];
+// Product_data.forEach(function (value, index)
+//  {
 
 
 
 
 
+//  else if ((value.product_discount_percenteage <= 25 && value.product_discount_percenteage >10) && value.cat_id == "3")
+// 	{
+// 		offer10Arr.push(value);
+// 		$("#outerDiv1").append(`
+// 		<div class="item" style="width:275px;display:inline-block;overflow:visible;cursor:pointer">
+//                     <div class="pad15">
+// 					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
+// 					<p><b>${value.product_name}</b></p>                        
+//                         <p>${value.product_discount_percenteage}%</p>
+// 						<p>₹ ${value.product_discount_price}</p>
+// 						<p>${value.product_description}</p>
+//                     </div>
+//                 </div>
+
+// 	`)
 
 
 
-$(document).ready(function () {
-    var itemsMainDiv = ('.MultiCarousel');
-    var itemsDiv = ('.MultiCarousel-inner');
-    var itemWidth = "";
+// 	} else if
+// 		((value.product_discount_percenteage <=50  && value.product_discount_percenteage >25) && value.cat_id == "3")
+// 	{
+// 		offer10Arr.push(value);
+// 		$("#outerDiv2").append(`
+// 		<div class="item" style="width:275px;display:inline-block;overflow:visible;cursor:pointer">
+//                     <div class="pad15">
+// 					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
+// 					<p><b>${value.product_name}</b></p>                        
+//                         <p>${value.product_discount_percenteage}%</p>
+// 						<p>₹ ${value.product_discount_price}</p>
+// 						<p>${value.product_description}</p>
+//                     </div>
+//                 </div>
 
-    $('.leftLst, .rightLst').click(function () {
-        var condition = $(this).hasClass("leftLst");
-        if (condition)
-            click(0, this);
-        else
-            click(1, this)
-    });
+// 	`)
+// 	}else if((value.product_discount_percenteage <=70  && value.product_discount_percenteage >50) && value.cat_id == "3")
+// 	{
+// 		offer10Arr.push(value);
+// 		$("#outerDiv3").append(`
+// 		<div class="item" style="width:275px;display:inline-block;overflow:visible;cursor:pointer">
+//                     <div class="pad15">
+// 					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
+// 					<p><b>${value.product_name}</b></p>                        
+//                         <p>${value.product_discount_percenteage}%</p>
+// 						<p>₹ ${value.product_discount_price}</p>
+// 						<p>${value.product_description}</p>
+//                     </div>
+//                 </div>
 
-    // ResCarouselSize();
-
-
-
-
-    // $(window).resize(function () {
-    //     ResCarouselSize();
-    // });
-
-    // //this function define the size of the items
-    // function ResCarouselSize() {
-    //     var incno = 0;
-    //     var dataItems = ("data-items");
-    //     var itemClass = ('.item');
-    //     var id = 0;
-    //     var btnParentSb = '';
-    //     var itemsSplit = '';
-    //     var sampwidth = $(itemsMainDiv).width();
-    //     var bodyWidth = $('body').width();
-    //     $(itemsDiv).each(function () {
-    //         id = id + 1;
-    //         var itemNumbers = $(this).find(itemClass).length;
-    //         btnParentSb = $(this).parent().attr(dataItems);
-    //         itemsSplit = btnParentSb.split(',');
-    //         $(this).parent().attr("id", "MultiCarousel" + id);
+// 	`)
+// 	}else if((value.product_discount_percenteage >70) && value.cat_id == "3")
+// 	{
+// 		offer10Arr.push(value);
 
 
-    //         if (bodyWidth >= 1200) {
-    //             incno = itemsSplit[3];
-    //             itemWidth = sampwidth / incno;
-    //         }
-    //         else if (bodyWidth >= 992) {
-    //             incno = itemsSplit[2];
-    //             itemWidth = sampwidth / incno;
-    //         }
-    //         else if (bodyWidth >= 768) {
-    //             incno = itemsSplit[1];
-    //             itemWidth = sampwidth / incno;
-    //         }
-    //         else {
-    //             incno = itemsSplit[0];
-    //             itemWidth = sampwidth / incno;
-    //         }
-    //         $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
-    //         $(this).find(itemClass).each(function () {
-    //             $(this).outerWidth(itemWidth);
-    //         });
-
-    //         $(".leftLst").addClass("over");
-    //         $(".rightLst").removeClass("over");
-
-    //     });
-    // }
-
-
-    // //this function used to move the items
-    // function ResCarousel(e, el, s) {
-    //     var leftBtn = ('.leftLst');
-    //     var rightBtn = ('.rightLst');
-    //     var translateXval = '';
-    //     var divStyle = $(el + ' ' + itemsDiv).css('transform');
-    //     var values = divStyle.match(/-?[\d\.]+/g);
-    //     var xds = Math.abs(values[4]);
-    //     if (e == 0) {
-    //         translateXval = parseInt(xds) - parseInt(itemWidth * s);
-    //         $(el + ' ' + rightBtn).removeClass("over");
-
-    //         if (translateXval <= itemWidth / 2) {
-    //             translateXval = 0;
-    //             $(el + ' ' + leftBtn).addClass("over");
-    //         }
-    //     }
-    //     else if (e == 1) {
-    //         var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
-    //         translateXval = parseInt(xds) + parseInt(itemWidth * s);
-    //         $(el + ' ' + leftBtn).removeClass("over");
-
-    //         if (translateXval >= itemsCondition - itemWidth / 2) {
-    //             translateXval = itemsCondition;
-    //             $(el + ' ' + rightBtn).addClass("over");
-    //         }
-    //     }
-    //     $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
-    // }
-
-    //It is used to get some elements from btn
-    function click(ell, ee) {
-        var Parent = "#" + $(ee).parent().attr("id");
-        var slide = $(Parent).attr("data-slide");
-        ResCarousel(ell, Parent, slide);
-    }
-
-});
+// 	`)
+// // 	}
+// // });
 
 
 
+let offer = [];
 
-/*......sidebar responsive...*/
-// function openNav() {
-// 	document.getElementById("myNav").style.display = "block";
-//   }
+	$('.listValue').click(function () {
+		alert('hello')
+		Product_data.forEach(function (value, index) {
+		const dataOffer = $(this).attr('DataItem');
+		
+			if ((value.product_discount_percenteage <= dataOffer || value.product_discount_percenteage >=
+				 (dataOffer-10)) && value.cat_id == "3") {
+				offer.push(value);
 
-//   function closeNav() {
-// 	document.getElementById("myNav").style.display = "none";
-//   }
+				$("#outerDiv").append(`
 
-// $(document).load($(window).bind("resize", checkPosition));
-
-// function checkPosition() {
-// 	if ($(window).width() < 850) {
-// 		$("#mysidepnav").css('display', 'none');
-// 		$('.pages').removeClass('col-9');
-// 		$('.pages').addClass('col-12');
-// 		$('#icon').css('width', 'block');
-
-// 		$('#icon').click(function () {
-// 			$('#mysidepnav').css('display', 'inline-block');
-// 		})
-// 	}
-// 	else {
-// 		$("#mysidepnav").css('display', 'block');
-// 		$('.pages').removeClass('col-12');
-// 		$('.pages').addClass('col-9');
-// 		$('#icon').css('display', 'none');
-// 	}
-// }
- let offer10Arr = [];
-Product_data.forEach(function (value, index)
- {
-	if (value.product_discount_percenteage <= "10" && value.cat_id =="3")
-	{
-		offer10Arr.push(value);
-		$("#outerDiv").append(`
-
-		<div class="item" style="width:275px;display:inline-block;">
+		<div class="item" style="width:275px;display:inline-block;cursor:pointer;">
                     <div class="pad15">
 					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
 					<p><b>${value.product_name}</b></p>                        
-                        <p>${value.product_discount_percenteage}%</p>
+                        <p>${value.product_discount_percenteage}% off</p>
 						<p>₹ ${value.product_discount_price}</p>
 						<p>${value.product_description}</p>
                     </div>
                 </div>
 	`)
-		
-		
-	}else if ((value.product_discount_percenteage <= 25 && value.product_discount_percenteage >10) && value.cat_id == "3")
-	{
-		offer10Arr.push(value);
-		$("#outerDiv1").append(`
-		<div class="item" style="width:275px;display:inline-block;overflow:visible;">
-                    <div class="pad15">
-					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
-					<p><b>${value.product_name}</b></p>                        
-                        <p>${value.product_discount_percenteage}%</p>
-						<p>₹ ${value.product_discount_price}</p>
-						<p>${value.product_description}</p>
-                    </div>
-                </div>
-
-	`)
-		
-		
-	
-	} else if
-		((value.product_discount_percenteage <=50  && value.product_discount_percenteage >25) && value.cat_id == "3")
-	{
-		offer10Arr.push(value);
-		$("#outerDiv2").append(`
-		<div class="item" style="width:275px;display:inline-block;overflow:visible;">
-                    <div class="pad15">
-					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
-					<p><b>${value.product_name}</b></p>                        
-                        <p>${value.product_discount_percenteage}%</p>
-						<p>₹ ${value.product_discount_price}</p>
-						<p>${value.product_description}</p>
-                    </div>
-                </div>
-
-	`)
-	}else if((value.product_discount_percenteage <=75  && value.product_discount_percenteage >50) && value.cat_id == "3")
-	{
-		offer10Arr.push(value);
-		$("#outerDiv3").append(`
-		<div class="item" style="width:275px;display:inline-block;overflow:visible;">
-                    <div class="pad15">
-					<p class="lead"><img  style="width:100%;" src="${value.product_img}" alt=""></p>
-					<p><b>${value.product_name}</b></p>                        
-                        <p>${value.product_discount_percenteage}%</p>
-						<p>₹ ${value.product_discount_price}</p>
-						<p>${value.product_description}</p>
-                    </div>
-                </div>
-
-	`)
-	}
-});
 
 
+			
+
+
+		}
+	})
+
+
+})
 
 
 
