1cca4d43495ba0b9c1aac3e97c69ee443ced7526
aashu89 <aashutosh.namdeo@codesquaretech.com>
2022-10-27 18:27:27 +0530

    aa

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
     type: 'GET',
     crossDomain: true,
     headers: {'Content-Type': 'application/json' ,  'Access-Control-Allow-Origin': '*' , 'Access-Control-Allow-Origin': '*' , "Access-Control-Allow-Credentials" : "true" , "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT" , "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"},
-    beforeSend: function(xhr){
-      xhr.withCredentials = true;
-    },
     success: function(data){
       console.log(data);
     },
