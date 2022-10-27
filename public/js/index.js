7e63564c9d9a438815d10abd7f9e46c7191178d2
aashu89 <aashutosh.namdeo@codesquaretech.com>
2022-10-27 18:29:37 +0530

    aa

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
   $.ajax({
     url: url,
     type: 'GET',
-    crossDomain: true,
     headers: {'Content-Type': 'application/json' ,  'Access-Control-Allow-Origin': '*' , 'Access-Control-Allow-Origin': '*' , "Access-Control-Allow-Credentials" : "true" , "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT" , "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"},
     success: function(data){
       console.log(data);
