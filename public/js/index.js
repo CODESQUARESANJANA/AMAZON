e9a935e01112e7c8f1700fd13dbb8a76bcb62b9c
aashu89 <aashutosh.namdeo@codesquaretech.com>
2022-11-02 17:27:39 +0530

    aa

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     }
   })
 
-  
+  getAccessToken('https://bjxc-002.sandbox.us01.dx.commercecloud.salesforce.com', '88186553-367a-4c61-8161-992902296e76' , 'Basic YWFzaHV0b3NoLm5hbWRlb0Bjb2Rlc3F1YXJldGVjaC5jb206QWFzaHVAY29kZXNxdWFyZTEyMzpjb2Rlc3F1YXJlMjAyMg==')
     $('#topHeader').load('topHeader.html');
     $('#footer').load('footer.html');
     $('#my-carousel-header').load('responsive.html');
@@ -191,7 +191,7 @@ function getAccessToken(baseUrl , clientId , basicToken){
     url: url,
     type: 'POST',
     crossDomain: true,
-    headers: {'Authorization': basicToken , 'Content-Type': 'application/x-www-form-urlencoded' ,  'Access-Control-Allow-Origin': '*'  , "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT" , "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"},
+    headers: {'Authorization': basicToken , 'Content-Type': 'application/x-www-form-urlencoded' },
     beforeSend: function(xhr){
       xhr.withCredentials = true;
     },
